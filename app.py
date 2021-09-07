2f6dad40ceefc7dacb581c9d41b50f6813d05d55
Fuad Rustamzade <fuadrustamzade@Fuads-Air.STELFIBER>
2021-09-07 20:03:53 +0400

    created first endpoint

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-#first commit test
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+
+
+
+class Hello(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name')
+    def get(self, name):
+        return name
+api = Api(app)
+
+api.add_resource(Hello, '/home/<string:name>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
